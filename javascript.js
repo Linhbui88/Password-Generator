d3f267c84a2729e932c0de97e84463e750389ea8
LinhBui88 <87442413+Linhbui88@users.noreply.github.com>
2021-08-15 00:05:33 -0400

    finish js

--- javascript.js
+++ javascript.js
@@ -1,108 +1,145 @@
 // Assignment Code
+var specialChars = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];;
+var numbers  = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+var lowerChars = 'abcdefghijklmnopqrstuvwxyz'
+var upperChars = lowerChars.toUpperCase()
 
-// Start Password variable values:
-
-specialChars =["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];;
-numbers  = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-lowerChars = 'abcdefghijklmnopqrstuvwxyz'
-upperChars = lowerChars.toUpperCase()
-
-var password = [ 'specialChars', 'numbers', 'lowerChars','upperChars']
-
-function getRandomUpper() { 
-    return upperChars[Math.floor(Math.random()*lowerChars.length)]
+function getRandomUseableChar(useableChars) {
+ return useableChars[Math.floor(Math.random() * useableChars.length)]
 }
-function getRandomLower(){
-    return lowerChars[Math.floor(Math.random() * lowerChars.length)]
-}
-
-function getRandomNumber() {
-    return numbers[Math.floor(Math.random() * numbers.length)]
-}
-
-function getRandomSpecialChars() {
-    return specialChars[Math.floor(Math.random() * specialChars.length)]
-}
-
-var hasUpper = getRandomUpper()
-var hasLower = getRandomLower()
-var hasNumber = getRandomNumber()
-var hasSpecialChars = getRandomSpecialChars()
 
 var generateBtn = document.querySelector('#generate');
 generateBtn.addEventListener('click', function() {
-pw = generatePassword();
-DocumentTimeline.getElementById('password').placeholder = pw;
+    var passwordLength = prompt("How many characters would you like your password? Choose between 8 and 128");
+
+    if(!passwordLength) {
+        alert('You must enter a value');
+    }  else if (passwordLength < 8 || passwordLength > 128) {
+        passwordLength = prompt('You must choose between 8 and 128');
+    }
+    else {
+        var hasNumber = confirm('Will this contain numbers?')
+        var hasSpecialChars = confirm("Will this contain special characters?");
+        var hasUpper= confirm("Will this contain Uppercase letters?");
+        var hasLower = confirm("Will this contain Lowercase letters?");
+
+        // GENERATE PASSWORD
+        var generatePassword = ''
+        var useableCharacters = ''
+        if( hasUpper === true) {
+            useableCharacters = useableCharacters + upperChars
+        }
+        if ( hasSpecialChars === true) {
+            useableCharacters = useableCharacters + specialChars.join('')
+        }
+        if(hasLower === true) {
+            useableCharacters = useableCharacters + lowerChars
+
+        }
+        if(hasNumber === true) {
+            useableCharacters = useableCharacters + numbers.join('')
+        }
+
+        console.log(useableCharacters)
+
+        for (var i = 0; i < passwordLength; i++) {
+            var randomUseableChar = getRandomUseableChar(useableCharacters)
+            generatePassword = generatePassword + randomUseableChar
+        }   
+
+       var passwordText = document.querySelector("#password");
+        passwordText.value = generatePassword;
+
+    }
+
 });
 
-var enter
-function generatePassword() {
-enter = prompt("How many characters would you like your password? Choose between 8 and 128");
-if(!enter) {
-    alert('You must enter a value');
-}  else if (enter < 8 || enter > 128) {
-    enter = prompt('You must choose between 8 and 128');
-}
+
+
+
+
+
+
+
+// var hasUpper = getRandomUpper()
+// var hasLower = getRandomLower()
+// var hasNumber = getRandomNumber()
+// var hasSpecialChars = getRandomSpecialChars()
+
+// var generateBtn = document.querySelector('#generate');
+// generateBtn.addEventListener('click', function() {
+// pw = generatePassword();
+// document.getElementById('password').placeholder = pw;
+// });
+
+// var enter
+// function generatePassword() {
+// enter = prompt("How many characters would you like your password? Choose between 8 and 128");
+// if(!enter) {
+//     alert('You must enter a value');
+// }  else if (enter < 8 || enter > 128) {
+//     enter = prompt('You must choose between 8 and 128');
+// }
     
-   else {
-       hasNumber = confirm('Will this contain numbers?')
-       hasSpecialChars = confirm("Will this contain special characters?");
-       hasUpper= confirm("Will this contain Uppercase letters?");
-       hasLower = confirm("Will this contain Lowercase letters?");
-       };
+//    else {
+//        hasNumber = confirm('Will this contain numbers?')
+//        hasSpecialChars = confirm("Will this contain special characters?");
+//        hasUpper= confirm("Will this contain Uppercase letters?");
+//        hasLower = confirm("Will this contain Lowercase letters?");
+//        };
 
-}
-if (!hasLower && !hasUpper && !hasNumber && !hasSpecialChars) {
-    choices = alert("You must choose a criteria!");
+// }
 
-} else if(hasLower && hasUpper && hasNumber && hasSpecialChars) {
-    choices = hasSpecialChars.concat(hasUpper,hasLower,hasNumber);
+// if (!hasLower && !hasUpper && !hasNumber && !hasSpecialChars) {
+//     choices = alert("You must choose a criteria!");
 
-    // Else if for 3 positive options
-} else if(hasUpper && hasNumber && hasSpecialChars) {
-    choices = hasUpper.concat(hasNumber,hasSpechialChars);
+// } else if(hasLower && hasUpper && hasNumber && hasSpecialChars) {
+//     choices = hasSpecialChars.concat(hasUpper,hasLower,hasNumber);
 
-}  else if(hasLower&& hasNumber && hasSpecialChars) {
-    choices = hasLower.concat(hasNumber,hasSpechialChars);
-}
-    else if (hasLower && hasUpper && hasSpecialChars) {
-    choices = hasLower.concat(hasUpper,hasSpechialChars);
-}
-    else if (hasLower && hasUpper && hasNumber ) {
-    choices = haslower.concat(hasUpper,hasNumber);
-}
-// Else if for 2 positive option
-    else if (hasLower && hasUpper) {
-    choices=hasLower.concat(hasUpper);
-}
-    else if (hasLower && hasNumber) {
-    choices = hasLower.concat(hasNumber);
-}
-    else if (hasLower && hasSpecialChars) {
-    choices = hasLower.concat(hasSpechialChars);
-}
-    else if (hasUpper && hasNumber) {
-    choices = hasUpper.concat(hasNumber);
-}
-    else if (hasUpper && hasSpecialChars) {
-    choices = hasUpper.concat(hasSpecialChars);
-}
-// Else if for 1 positive option
-    else if (hasSpecialChars) {
-    choices = hasSpecialChars;
-}
-    else if (hasUpper) {
-    choices = hasUpper;
-}
-    else if (hasLower) {
-    choices = hasLower;
-}
-    else if (hasNumber) {
-    choices = hasNumber;
-}
+//     // Else if for 3 positive options
+// } else if(hasUpper && hasNumber && hasSpecialChars) {
+//     choices = hasUpper.concat(hasNumber,hasSpechialChars);
 
+// }  else if(hasLower&& hasNumber && hasSpecialChars) {
+//     choices = hasLower.concat(hasNumber,hasSpechialChars);
+// }
+//     else if (hasLower && hasUpper && hasSpecialChars) {
+//     choices = hasLower.concat(hasUpper,hasSpechialChars);
+// }
+//     else if (hasLower && hasUpper && hasNumber ) {
+//     choices = haslower.concat(hasUpper,hasNumber);
+// }
+// // Else if for 2 positive option
+//     else if (hasLower && hasUpper) {
+//     choices=hasLower.concat(hasUpper);
+// }
+//     else if (hasLower && hasNumber) {
+//     choices = hasLower.concat(hasNumber);
+// }
+//     else if (hasLower && hasSpecialChars) {
+//     choices = hasLower.concat(hasSpechialChars);
+// }
+//     else if (hasUpper && hasNumber) {
+//     choices = hasUpper.concat(hasNumber);
+// }
+//     else if (hasUpper && hasSpecialChars) {
+//     choices = hasUpper.concat(hasSpecialChars);
+// }
+// // Else if for 1 positive option
+//     else if (hasSpecialChars) {
+//     choices = hasSpecialChars;
+// }
+//     else if (hasUpper) {
+//     choices = hasUpper;
+// }
+//     else if (hasLower) {
+//     choices = hasLower;
+// }
+//     else if (hasNumber) {
+//     choices = hasNumber;
+// }
 
+// console.log(choices)
 
 
 
